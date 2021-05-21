dff35cea38d73febe149f8dd576bd5586f5e1fb4
yumauma20 <71yuma10@gmail.com>
2021-05-21 23:24:18 +0900

    予定作成フォームから送られた情報を保存

--- routes/schedules.js
+++ routes/schedules.js
@@ -2,14 +2,33 @@
 const express = require('express');
 const router = express.Router();
 const authenticationEnsurer = require('./authentication-ensurer');
+const uuid = require('uuid');
+const Schedule = require('../models/schedule');
+const Candidate = require('../models/candidate');
 
 router.get('/new', authenticationEnsurer, (req, res, next) => {
   res.render('new', { user: req.user });
 });
 
 router.post('/', authenticationEnsurer, (req, res, newt) => {
-  console.log(req.body); // TODO　予定と候補を保存する実装をする
-  res.redirect('/');
+  const scheduleId = uuid.v4();
+  const updatedAt = new Date();
+  Schedule.create({
+    scheduleId: scheduleId,
+    scheduleName: req.body.scheduleName.slice(0, 255) || '(名称未設定)',
+    memo: req.body.memo,
+    createBy: req.user.id,
+    updatedAt: updatedAt
+  }).then((schedule) => {
+    const candidateNames = req.body.candidates.trim().split('\n').map((s) => s.trim()).filter((s) => s !== "");
+    const candidates = candidateNames.map((c) => { return {
+      candidateName: c,
+      scheduleId: schedule.scheduleId
+    };});
+    Candidate.bulkCreate(candidates).then(() =>  {
+      res.redirect('/schedules/' + schedule.scheduleId);
+    });
+  });
 });
 
 module.exports = router;
